fd6fea2221ae2280ca314d5376879b1286c474ee
igorfariasoliveira <igor10farias@gmail.com>
2025-11-17 08:57:25 -0300

    Dasboard

--- frota-pro/src/app/app.routes.ts
+++ frota-pro/src/app/app.routes.ts
@@ -10,5 +10,11 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () =>
       import('./features/auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard/dashboard')
+        .then(m => m.Dashboard)
   }
 ];
