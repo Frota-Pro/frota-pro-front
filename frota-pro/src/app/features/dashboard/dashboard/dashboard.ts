1dae725abe06e7e4848eb6e3ef837e7fb3e11673
igorfariasoliveira <igor10farias@gmail.com>
2025-11-19 15:27:56 -0300

    att

--- frota-pro/src/app/features/dashboard/dashboard/dashboard.ts
+++ frota-pro/src/app/features/dashboard/dashboard/dashboard.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './dashboard.html',
-  styleUrls: ['./dashboard.css']
+  styleUrls: ['./dashboard.css'],
 })
 export class Dashboard {
   isClosed = false;
@@ -14,8 +14,13 @@ export class Dashboard {
     this.isClosed = !this.isClosed;
   }
   statusFrota = {
-  total: 25,
-  emRota: 8,
-  manutencao: 3
-};
+    total: 25,
+    emRota: 8,
+    manutencao: 3,
+  };
+  notificacoes = [
+    { tipo: 'alerta', mensagem: 'Caminhão ABC-1234 está há 5 dias sem movimentação.' },
+    { tipo: 'risco', mensagem: 'Caminhão XYZ-8899 excedeu o limite de km diário.' },
+    { tipo: 'aviso', mensagem: 'Caminhão FGH-5521 está há 12 dias na oficina.' },
+  ];
 }
