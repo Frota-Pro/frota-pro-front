1b1a8aa6baec6001b1eb450966c767534c159e6d
igorfariasoliveira <igor10farias@gmail.com>
2025-12-02 08:19:33 -0300

    Corrigindo erro

--- frota-pro/src/app/features/veiculos/veiculos.component.ts
+++ frota-pro/src/app/features/veiculos/veiculos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 type UUID = string;
 
