3eff7f754360dac47715b322c6efb5950726fd00
igorfariasoliveira <igor10farias@gmail.com>
2025-12-16 08:32:42 -0300

    Manuntenção

---
+++ frota-pro/src/app/features/manuntencao/manuntencao.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Manuntencao } from './manuntencao.component';
+
+describe('Manuntencao', () => {
+  let component: Manuntencao;
+  let fixture: ComponentFixture<Manuntencao>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Manuntencao]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Manuntencao);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frota-pro/src/app/features/manuntencao/manuntencao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manuntencao',
+  imports: [],
+  templateUrl: './manuntencao.component.html',
+  styleUrl: './manuntencao.component.css',
+})
+export class Manuntencao {
+
+}
